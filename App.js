511a9a479f0dcf79a0149cf1a8b6b550586cd49f
dmardoqueu <diogomardoqueu@gmail.com>
2024-11-20 17:59:36 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,80 @@
+const Database = require("./Database")
+const Author = require("./entities/Author")
+const Book = require("./entities/Book")
+const Order = require("./entities/Order")
+const Poster = require("./entities/Poster")
+const Product = require("./entities/Product")
+const User = require("./entities/Users")
+
+module.exports = class App {
+    static #database = new Database()
+
+    createUser(name, email, password) {
+        const user = new User(name, email, password)
+        App.#database.saveUser(user)
+    }
+
+    getUsers() {
+        return App.#database.find('users')
+    }
+
+    createUser(name, email, password) {
+        const user = new User(name, email, password)
+        App.#database.saveUser(user)
+    }
+
+    createAuthor(name, nationality, bio) {
+        const author = new Author(name, nationality, bio)
+        App.#database.saveAuthor(author)
+    }
+
+    getAuthors() {
+        return App.#database.find('authors')
+    }
+
+    createBook(title, synopsis, genre, pages, author, description, price, inStock) {
+        const book = new Book(title, synopsis, genre, pages, author, description, price, inStock)
+        App.#database.saveBook(book)
+    }
+
+    getBooks() {
+        return App.#database.find('books')
+    }
+
+    createPoster(name, description, height, width, price, inStock) {
+        const poster = new Poster(name, description, height, width, price, inStock)
+        App.#database.savePoster(poster)
+    }
+
+    getPosters() {
+        return App.#database.find('posters')
+    }
+
+    addBook(bookName, quantity) {
+        App.#database.addBooksToStock(bookName, quantity)
+    }
+
+    addPoster(posterName, quantity) {
+        App.#database.addPostersToStock(posterName, quantity)
+    }
+
+    createOrder(items, user) {
+        const order = new Order(items, user)
+        App.#database.saveOrder(order)
+        order.getOrder.items.forEach(({ product, quantity }) => {
+            if (product instanceof Book) {
+                App.#database.removeBooksFromStock(product.name, quantity)
+            } else {
+                App.#database.removePostersFromStock(product.name, quantity)
+            }
+        })
+    }
+
+    getOrders() {
+        return App.#database.find('orders')
+    }
+
+    showDatabase() {
+        App.#database.showStorage()
+    }
+}
